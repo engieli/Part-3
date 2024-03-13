434cc3a314fce4633c9beed1e55f00abaf6ad691
Eng Sathorn <sathorn@shernet.sheridancollege.ca>
2024-03-13 12:00:33 -0400

    task 4

---
+++ Assets/Week 9/Scripts/Thief.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : Villager
+{
+
+    public GameObject knifePrefab;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
+    public float moveSpeed = 5f;
+
+
+    public override ChestType CanOpen()
+    {
+        return ChestType.Thief;
+    }
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Instantiate(knifePrefab, spawnPoint2.position, spawnPoint2.rotation);
+        Instantiate(knifePrefab, spawnPoint3.position, spawnPoint3.rotation);
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            Vector2 targetPosition = GetMouseWorldPosition();
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private Vector2 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
